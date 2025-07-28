9671d49ffbc51a6355682dd56d4f785a6cb7272b
renan <renan.br3@gmail.coml>
2025-07-28 13:53:59 -0300

    commit, creation of user registration and tables in the database

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models 
 from django.contrib.auth.models import User
 
+
 class Account(models.Model):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -9,3 +10,25 @@ class Account(models.Model):
 
     def __str__(self):
         return self.name
+
+from decimal import Decimal
+
+class Tree(models.Model):
+    name = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class PlantedTree(models.Model):
+    tree = models.ForeignKey(Tree, on_delete=models.CASCADE, related_name='plantings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    age = models.PositiveIntegerField(default=0)  # idade da árvore em anos
+    planted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tree.name} planted by {self.user.username}"
+
+
