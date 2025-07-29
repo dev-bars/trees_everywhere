7a66f367253e6c86d0aebce58cdaa42f13b441b6
renan <renan.br3@gmail.coml>
2025-07-29 11:01:03 -0300

    commit, inserted link details of planted trees

--- core/urls.py
+++ core/urls.py
@@ -14,3 +14,13 @@ urlpatterns = [
     path('', home, name='home'),
     path('minhas-arvores/', my_trees, name='my_trees'),
 ]
+
+
+from .views import planted_tree_detail
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', home, name='home'),
+    path('minhas-arvores/', my_trees, name='my_trees'),
+    path('arvore/<int:pk>/', planted_tree_detail, name='planted_tree_detail'),
+]
--- core/views.py
+++ core/views.py
@@ -20,5 +20,12 @@ def my_trees(request):
     planted_trees = PlantedTree.objects.filter(user=user)
     return render(request, 'core/my_trees.html', {'planted_trees': planted_trees})
 
+from django.shortcuts import get_object_or_404
+
+@login_required
+def planted_tree_detail(request, pk):
+    tree = get_object_or_404(PlantedTree, pk=pk, user=request.user)
+    return render(request, 'core/planted_tree_detail.html', {'tree': tree})
+
 
 
